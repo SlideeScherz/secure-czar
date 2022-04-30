62275699742755c358cafa18bf42a473fe38a5db
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-04-30 15:48:56 -0400

    swap ada95 with vb.net

---
+++ caesar-cipher.vb
@@ -0,0 +1,47 @@
+Imports System
+
+Module Program
+
+  'Helper function to cipher one char of a string array in a (forEach / Map)
+  Private Function ShiftChar(charIn As Char, nShifts As Integer) As Char
+
+    ' handle upper or lower case
+    Dim lCase = If(Asc(charIn) <= 90, 65, 97)
+
+    Return Chr(lCase + (((Asc(charIn) + nShifts - lCase) Mod 26)))
+  End Function
+
+  'Encrypt a string with a caesar cipher
+  Private Function Encrypt(strIn As String, nShifts As Integer) As String
+
+    ' handle a large shift amount */
+    Dim k As Integer = If(nShifts >= 26, nShifts Mod 26, nShifts)
+
+    Return New String(strIn.Select(Function(el) ShiftChar(el, k)).ToArray())
+
+  End Function
+
+  'Decrypt a string with a caesar cipher
+  Private Function Decrypt(strIn As String, nShifts As Integer) As String
+
+    Dim k As Integer = If(nShifts >= 26, nShifts Mod 26, nShifts)
+
+    Return Encrypt(strIn, 26 - k)
+
+  End Function
+
+  Sub Main(args As String())
+
+    Const inputString As String = "bigShift"
+    Const shifts As Integer = 10000
+
+    Dim ciphered As String = Encrypt(InputString, shifts)
+    Dim deciphered As String = Decrypt(ciphered, shifts)
+
+    Console.WriteLine(inputString + " " + Str(shifts))
+    Console.WriteLine(ciphered)
+    Console.WriteLine(deciphered)
+
+  End Sub
+
+End Module
